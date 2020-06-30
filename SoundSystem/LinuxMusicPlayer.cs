8a77024868e361303ba80bb1d64222f6e5f0c352
Kieran T Brawley <ktb.developer@gmail.com>
2020-06-30 09:56:42 +0100

    primitate volume fade in/out for linux player

--- SoundSystem/LinuxMusicPlayer.cs
+++ SoundSystem/LinuxMusicPlayer.cs
@@ -2,23 +2,40 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Threading;
 
 namespace SaveRoomCP.SoundSystem
 {
     public class LinuxMusicPlayer : IPlayer
     {
         private readonly string MusicBasePath = $"{Environment.CurrentDirectory}/SaveRoomMusic";
+        private const int TIMEOUT_DELTA = 50;
 
         private Process _process;
 
         public Task Play(string fileName)
         {
             _process = (StartAplayPlayback(fileName));
+            for (int i = 0; i < 100; i++)
+            {
+                AdjustVolume(true);
+                Thread.Sleep(TIMEOUT_DELTA);
+            }
+            
             return Task.CompletedTask;
         }
 
         public Task Stop()
         {
+            Console.WriteLine();
+            Console.WriteLine("Stopping Music...");
+            Console.WriteLine();
+
+            for (int i = 100; i > 0; i--)
+            {
+                AdjustVolume(false);
+                Thread.Sleep(TIMEOUT_DELTA);
+            }
             if (_process != null)
             {
                 _process.Kill();
@@ -46,6 +63,24 @@ namespace SaveRoomCP.SoundSystem
             };
             process.Start();
             return process;
+        }
+
+        private void AdjustVolume(bool increaseVol)
+        {
+            string volumeDelta = increaseVol ? "1%+" : "1%-";
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"amixer -q sset Master {volumeDelta}",
+                    RedirectStandardOutput = true,
+                    RedirectStandardInput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
         }
     }
 }
